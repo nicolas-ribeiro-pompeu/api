a3ca8e15a9148b294565414b7c76c16cd6af68f5
Nicolas Ribeiro Pompeu Glória <nicolasribeiropompeu@gmail.com>
2026-04-09 17:50:10 -0300

    path variable, request param e request header

--- src/main/java/com/example/treinoapi/MeuController.java
+++ src/main/java/com/example/treinoapi/MeuController.java
@@ -3,22 +3,29 @@ package com.example.treinoapi;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/Bem-Vindo")
 public class MeuController {
 
     @GetMapping
-    public String nome() {
-        return "testando";
+    public String BemVIndo() {
+        return "Bem Vindo";
     }
 
-    @GetMapping("/ola")
-    public String ola() {
-        return "Olá";
+    @GetMapping("Teste-RequestParam")
+    public long primeiroExemplo (@RequestParam long id)
+    {System.out.println("ID recebido: " + id);
+        return id;
     }
 
-    @GetMapping ("/Bemvindo")
-public String bemvindo (){
-        return "bem vindo";
+    @GetMapping ("Teste-PathVariable/id/{id}")
+public long SegundoExemplo (@PathVariable("id")long id)
+    {System.out.println("ID recebido: " + id);
+        return id;
+}
+@GetMapping ("Teste-RequestHeader")
+public String TerceiroExemplo (@RequestHeader("user-agent") String UserAgent, @RequestParam long id){
+    System.out.println("User-Agent: " + UserAgent);
+        return UserAgent;
 }
     @PostMapping
     public String criar(@RequestBody String nome) {
